62aaf96ef1a1570196163401b744958e880069a0
SGT <977.saugat@gmail.com>
2024-05-30 00:11:57 +0545

    Footer and header completer UI

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { FaCopyright } from 'react-icons/fa'
+import { LuCopyright } from 'react-icons/lu'
 
 const Footer = () => {
   return (
-    <div className='bg-blue-800 flex justify-center h-6 items-center pt-0'>
-        <div className='text-white flex h-8 bg-black gap-1 pt-3'><FaCopyright/>
+    <div className='bg-blue-800 flex justify-center items-center pt-0'>
+        <div className='text-white flex gap-1 items-center'><LuCopyright/>
 <div className='text-white text-3xl font-bold'/>Saugat Poudel 2024</div>
     </div>
   )
