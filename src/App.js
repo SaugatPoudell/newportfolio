8fb5664fdd2a5ed116528ea3b67695c658ddd218
SGT <977.saugat@gmail.com>
2024-05-30 23:01:18 +0545

    Working with it

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 
+import About from './components/About';
 import Footer from './components/Footer';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
@@ -6,11 +7,20 @@ import Navbar from './components/Navbar';
 function App() {
   return (
     <>
+<div className='top-0 -z-0 h-full w-full'>
+    <div className="relative inset-0 -z-10 h-full w-full items-center [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
 
-    <div className="absolute inset-0 -z-10 h-full w-full items-center [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
       <Navbar/>
-      <Footer/>
       <Hero/>
+      <About/>
+
+
+
+   
+      <Footer/>
+
+     
+    </div>
     </div>
     </>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,14 +5,15 @@ import logo from '../images/forweb.jpg';
 const Hero = () => {
   return (
     <>
-      <div className='flex justify-around bg-[#FF5733]'>
+      <div className='flex justify-around'>
         <div>
     <div className='text-white ml-20 mt-20 text-3xl lg:text-6xl lg:font-bold'>Saugat Poudel</div>
     <div className='text-2xl text-white ml-20 pt-5 gradiet lg:text-4xl'>Frontend Developer</div>
-    <p className='text-white mr-20 ml-20 w-500'>{mine_desc}</p>
+    <p className='text-white mr-20 ml-20 max-w-7xl tracking-tighter mt-10 mb-20 text-justify'>{mine_desc}</p>
     </div>
-    <div className='w-screen mt-20 mr-20'>
-      <img src={logo} alt='IDK'/>
+    <div className='container max-h-xl mt-20'>
+      
+      <img src={logo} className='h-[500px] rounded-3xl' alt='IDK'/>
     </div>
 
     </div>
