3a677c444312002fe036101a128c400e7bb956f8
SGT <977.saugat@gmail.com>
2024-05-31 23:59:50 +0545

    Animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,22 +6,23 @@ import { motion } from "framer-motion";
 const Hero = () => {
   return (
     <>
-      <div className='flex justify-around'>
+      <motion.div
+      initial={{x:-100,opacity:0}}
+       animate={{ x: 0,opacity:1 }}
+    transition={{ duration:0.5}}
+     className='flex justify-around'>
         <div>
     <div className='text-white ml-20 mt-20 text-3xl lg:text-6xl lg:font-bold'> <h2 > Saugat Poudel</h2></div>
     <div className='text-2xl text-white ml-20 pt-5 gradiet lg:text-4xl'>Frontend Developer</div>
-    <motion.p 
-    animate={{ x: 30 }}
-    transition={{ delay: 2 }}
+    <p 
      className='text-white mr-20 ml-20 max-w-7xl tracking-tighter mt-10 mb-20 text-justify'>{mine_desc}
-     </motion.p>
+     </p>
     </div>
-    <div className='container max-h-xl mt-20'>
-      
+    <div className='container max-h-l mt-20 mr-20'>
       <img src={logo} className='items-center lg:h-auto rounded-3xl' alt='IDK'/>
     </div>
 
-    </div>
+    </motion.div>
    
     </>
   )
--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { motion } from "framer-motion"
 import { FaHtml5 } from "react-icons/fa";
 import { FaCss3Alt } from "react-icons/fa6";
 import { FaReact } from "react-icons/fa";
@@ -7,14 +6,36 @@ import { SiFlutter } from "react-icons/si";
 import { TbBrandJavascript } from "react-icons/tb";
 import { SiAdobephotoshop } from "react-icons/si";
 import { SiAdobepremierepro } from "react-icons/si";
+import { motion } from 'framer-motion';
+
+const iconVarients =(duration)=>
+    ({
+            initial:{y:-10},
+            animate:{
+                y:[0,-10],
+                transition:{
+                duration:duration,
+                ease:"linear",
+                repeat:Infinity,
+                repeatType:"reverse"
+                }
+            }
+        }
+    );
 
 const Tech = () => {
   return (
     <div>
           <h2 className='text-white text-center font-mono font-bold text-2xl p-5'>Techonologies
             </h2>
-            <p className='flex text-white justify-center gap-1'>
-              <p className='text-orange-400 text-5xl'> <FaHtml5/></p> 
+            <motion.p variants={iconVarients(2)}
+            initial="initial"
+            animate="animate"
+             className='flex text-white justify-center gap-1'>
+              <motion.p variants={iconVarients(2)}
+            initial="initial"
+            animate="animate"
+            className='text-orange-400 text-5xl'> <FaHtml5/></motion.p> 
               <p className='text-blue-400 text-5xl'><FaCss3Alt/></p> 
              <p className='text-green-400 text-5xl'> < TbBrandJavascript/></p> 
              <p className='text-blue-800 text-5xl'>< SiFlutter/></p>
@@ -22,7 +43,7 @@ const Tech = () => {
               <p className='text-blue-400 text-5xl' >< SiAdobephotoshop /></p> 
               <p className='text-purple-400 text-5xl'>< SiAdobepremierepro/> </p>
 
-              </p>
+              </motion.p>
     </div>
   )
 }
